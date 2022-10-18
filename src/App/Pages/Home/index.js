25ba86a99f434d6d47a0cd7b65906e33d09da8e6
felipekaua <fkauadelima@gmail.com>
2022-10-18 19:02:03 -0300

    [add]First pages

--- src/App/Pages/Home/index.js
+++ src/App/Pages/Home/index.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./styles.scss";
+import { useNavigate } from "react-router";
+import container from "../../../assets/login/container.png";
 
 const Home = () => {
+
+    const navigate = useNavigate();
+    const [loginShop, setLoginShop] = useState(false);
+
+    function navigateToLoginClient(){
+      navigate("/loginClient");
+    }
+
     return(
       <>
-       <h1>Croissant</h1>
+        <div className="blackScreen"></div>
+        <div className="background"></div>
+        <div className="login">
+          <img src={container} alt="container" />
+          <div>
+            {loginShop ?
+              <>
+                <button>ADMIN</button>
+                <button>FUNCIONÁRIO</button>
+              </>
+              :
+              <>
+                <button onClick={navigateToLoginClient}>CLIENTE</button>
+                <button onClick={()=>{setLoginShop(true)}}>ESTABELECIMENTO</button>
+                
+              </>
+            }
+            
+          </div>
+        </div>
       </>
     )
   }
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import Home from './App/Pages/Home/index';
+import LoginClient from './App/Pages/LoginClient/index';
 
 export default function Router(){
   return (
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/loginClient" element={<LoginClient/>}/>
     </Routes>
   );
 };
